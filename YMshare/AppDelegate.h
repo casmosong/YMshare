cef094e6f1d4e6f0205d14c7f5d52c9e6dac6422
casmosong <song@songdemacbook-pro.local>
2016-01-05 09:24:06 +0800

    Initial Commit

---
+++ YMshare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YMshare
+//
+//  Created by song on 1/5/16.
+//  Copyright © 2016 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YMshare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YMshare
+//
+//  Created by song on 1/5/16.
+//  Copyright © 2016 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
